6589c7e84fd998d5d842c908eae5c882a171ba69
Mintri1199 <Mintri1199@github.com>
2018-09-23 20:12:24 -0700

    Page 5 of tutorial completed

--- Orange_Tree/GameScene.swift
+++ Orange_Tree/GameScene.swift
@@ -5,6 +5,12 @@ class GameScene: SKScene {
     var orange: Orange?
     var touchstart: CGPoint = .zero
     var shapeNode = SKShapeNode()
+    var boundary = SKNode()
+    
+    // Class method to load .sks files
+    static func Load (level: Int) -> GameScene?{
+        return GameScene(fileNamed: "Level-\(level)")
+    }
     
     
     override func didMove(to view: SKView) {
@@ -19,6 +25,18 @@ class GameScene: SKScene {
         
         // Set the contact delegate
         physicsWorld.contactDelegate = self
+        
+        // Setup the boundaries
+        boundary.physicsBody = SKPhysicsBody(edgeLoopFrom: CGRect(origin: .zero, size: size))
+        boundary.position = .zero
+        addChild(boundary)
+        
+        // Add the Sun to the scene
+        let sun = SKSpriteNode(imageNamed: "Sun")
+        sun.name = "sun"
+        sun.position.x = size.width - (sun.size.width * 0.75)
+        sun.position.y = size.height - (sun.size.height * 0.75)
+        addChild(sun)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -28,13 +46,25 @@ class GameScene: SKScene {
         
         // Check if the touch was on the Orange Tree
         if atPoint(location).name ==  "tree"{
-            // Create the orange and add it to the scene at the touhc location
+            // Create the orange and add it to the scene at the touch location
             orange = Orange()
             orange?.position =  location
             addChild(orange!)
             
             // Store the location of the touch
             touchstart = location
+        }
+        // Check whether the sun was tapped and change the level
+        for node in nodes(at: location) {
+            if node.name == "sun" {
+                let n = Int.random(in: 1 ... 2)
+                if let scene = GameScene.Load(level: n) {
+                    scene.scaleMode = .aspectFill
+                    if let view = view {
+                        view.presentScene(scene)
+                    }
+                }
+            }
         }
     }
     
@@ -59,8 +89,8 @@ class GameScene: SKScene {
         
         
         // Get the difference between the start and end point as a vector
-        let dx = touchstart.x - location.x
-        let dy = touchstart.y - location.y
+        let dx = (touchstart.x - location.x) * 0.5
+        let dy = (touchstart.y - location.y) * 0.5
         let vector = CGVector(dx: dx, dy: dy)
         
         // Set the Orange dynamic again and apply the vector as an impulse
